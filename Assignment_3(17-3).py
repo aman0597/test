47d3e0543ea1d5fed8faffc3c948e6d93f42169c
aman0597 <105729364+aman0597@users.noreply.github.com>
2022-05-17 19:48:33 +0530

    Add files via upload

---
+++ Assignment_3(17-3).py
@@ -0,0 +1,8 @@
+def addition():
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    sum = num1 + num2
+
+    print("The addition of two numbers is =",sum)
+
+addition()
